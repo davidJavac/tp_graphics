05414895ea6d71fc57113c7fc768fb58be47562f
davidJavac <43804211+davidJavac@users.noreply.github.com>
2019-06-09 16:14:12 -0300

    Se borra un comentario

--- src/model/ResizableImage.java
+++ src/model/ResizableImage.java
@@ -7,6 +7,5 @@ import unpaz.ayp3.bitmapDisplay.Pixel;
 public abstract class ResizableImage implements Valid, InclusiveBitmapDisplay{
 
 	public abstract Pixel[][] recorte(Pixel[][] imagen, int x1, int y1, int x2, int y2);
-	//public abstract BufferedImage reduccion(BufferedImage bf, int x1, int y1);
 	public abstract Pixel[][] reduccion(Pixel[][] imagen, int x1, int y1);
 }
